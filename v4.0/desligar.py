efddae629e049a8fa1bf25370908bced19acda64
Matheus Bibiano Alves <matheusbibiano30@gmail.com>
2021-11-23 20:12:03 -0300

    removendo v3.0 e v4.0 da entrega-1

--- v4.0/desligar.py
+++
@@ -1,7 +0,0 @@
-def desligarUrna():
-    """
-    Encerra o funcionomento da urna imprimindo
-    uma mensagem e fechando o terminal.
-    """
-    print("\n[*] Obrigado por utilizar a urna eletrônica!\n")
-    exit()
